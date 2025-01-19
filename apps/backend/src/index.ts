91640e60845540463fa19eab0038eeb14fb3181d
SoucaThomas <thomassouca@gmail.com>
2025-01-19 15:02:31 +0200

    created nodejs socketio backend

---
+++ apps/backend/src/index.ts
@@ -0,0 +1,15 @@
+import express from "express";
+import http from "http";
+import { Server } from "socket.io";
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+io.on("connection", (socket) => {
+    console.log("New client connected");
+    socket.on("disconnect", () => console.log("Client disconnected"));
+});
+
+const port = process.env.PORT || 4001;
+server.listen(port, () => console.log(`Listening on port ${port}`));
