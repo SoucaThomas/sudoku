51541dfb1589531ccdbdcb5df449fb7f329aec9c
SoucaThomas <thomassouca@gmail.com>
2025-01-28 19:51:25 +0200

    feat: refactor Sudoku grid rendering for improved layout and styling

--- apps/web/src/components/Game.tsx
+++ apps/web/src/components/Game.tsx
@@ -3,14 +3,14 @@ import { useState, useEffect } from "react";
 
 export default function Game() {
     const board =
-        "048301560360008090910670003020000935509010200670020010004002107090100008150834029";
+        "301086504046521070500000001400800002080347900009050038004090200008734090007208103";
 
     const grid = board.split("").map((cell) => parseInt(cell));
     const [selected, setSelected] = useState<number | null>(null);
 
-    const sameValue = "bg-yellow-800/10";
-    const sameRowCol = "bg-blue-200/30";
-    const sameSquare = "bg-purple-400/20";
+    const sameValue = "bg-[hsl(var(--primary))]/60";
+    const selectedCell = "bg-[hsl(var(--primary))]/90 border border-[hsl(var(--foreground))]";
+    const sameRowCol = "bg-[hsl(var(--primary))]/30";
 
     const handleMovement = (e: KeyboardEvent) => {
         switch (e.key) {
@@ -40,23 +40,38 @@ export default function Game() {
     return (
         <Card className="w-full h-full">
             <CardContent className="flex justify-center items-center h-full p-0 m-0">
-                <div className="aspect-square w-full max-w-md grid grid-cols-9 grid-rows-9 gap-1">
-                    {grid.map((cell, index) => (
-                        <Card
-                            key={index}
-                            className={`aspect-square w-full h-full text-center text-2xl font-bold rounded-none
-                              ${selected === index ? "border-white border" : ""}
-                              ${grid[selected] === grid[index] && grid[selected] != 0 ? sameValue : ""}
-                              ${Math.floor(selected / 9) === Math.floor(index / 9) && selected !== null ? sameRowCol : ""}
-                              ${selected !== null && selected % 9 === index % 9 ? sameRowCol : ""}
-                              ${Math.floor(selected / 27) === Math.floor(index / 27) && Math.floor((selected % 9) / 3) === Math.floor((index % 9) / 3) ? sameSquare : ""}
-                              `}
-                            style={{ cursor: "pointer" }}
-                            data-cell={index}
-                            onClick={() => setSelected(index)}
+                <div className="aspect-square w-full max-w-md grid grid-cols-3 grid-rows-3">
+                    {Array.from({ length: 9 }).map((_, box) => (
+                        <div
+                            className="w-full h-full border border-[hsl(var(--foreground))] grid grid-cols-3 grid-rows-3 text-center text-2xl font-bold"
+                            key={box}
                         >
-                            {cell === 0 ? "" : cell}
-                        </Card>
+                            {Array.from({ length: 9 }).map((_, index) => {
+                                const i =
+                                    Math.floor(box / 3) * 27 +
+                                    (box % 3) * 3 +
+                                    Math.floor(index / 3) * 9 +
+                                    (index % 3);
+                                return (
+                                    <Card
+                                        className={`rounded-none select-none 
+                                            ${grid[selected] === grid[i] && grid[selected] != 0 ? sameValue : ""}
+                                            ${selected !== null && selected % 9 === i % 9 ? sameRowCol : ""}
+                                            ${Math.floor(selected / 9) === Math.floor(i / 9) && selected !== null ? sameRowCol : ""}
+                                            ${Math.floor(selected / 27) === Math.floor(i / 27) && Math.floor((selected % 9) / 3) === Math.floor((i % 9) / 3) ? sameRowCol : ""}
+                                            ${selected === i ? selectedCell : ""}
+                                        `}
+                                        key={i}
+                                        onClick={() => {
+                                            setSelected(i);
+                                            console.log(selected);
+                                        }}
+                                    >
+                                        {grid[i] === 0 ? "" : grid[i]}
+                                    </Card>
+                                );
+                            })}
+                        </div>
                     ))}
                 </div>
             </CardContent>
