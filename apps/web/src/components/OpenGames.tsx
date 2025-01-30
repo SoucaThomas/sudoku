47d32eaf5c7bd8eadbcae496fe45feeb5ef81f0a
SoucaThomas <thomassouca@gmail.com>
2025-01-30 17:27:45 +0200

    feat: enhance OpenGames component with room availability message and improved layout

--- apps/web/src/components/OpenGames.tsx
+++ apps/web/src/components/OpenGames.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React from "react";
-import { Card, CardContent } from "./ui/card";
+import { Card, CardContent, CardTitle } from "./ui/card";
 import { askRooms, closeListenForRooms, listenForRooms } from "../actions/room";
 import { useEffect } from "react";
 import { GameRoom } from "@repo/socket.io-types";
@@ -34,62 +34,62 @@ export default function OpenGames() {
 
     return (
         <Card className="h-full w-full select-none overflow-y-scroll ">
-            <CardContent>
-                <Table>
-                    <TableHeader>
-                        <TableRow>
-                            <TableHead className="text-center">Name</TableHead>
-                            <TableHead>Game Mode</TableHead>
-                            <TableHead>Difficulty</TableHead>
-                            <TableHead>Players</TableHead>
-                            <TableHead></TableHead>
-                        </TableRow>
-                    </TableHeader>
-                    <TableBody>
-                        {loading ? (
+            <CardContent className="h-full w-full">
+                {rooms.length === 0 ? (
+                    <CardTitle className="w-full h-full flex items-center justify-center">
+                        No rooms available
+                    </CardTitle>
+                ) : (
+                    <Table>
+                        <TableHeader>
                             <TableRow>
-                                <TableCell colSpan={5} className="text-center">
-                                    <Spinner size="lg" className="bg-black dark:bg-white" />
-                                </TableCell>
+                                <TableHead className="text-center">Name</TableHead>
+                                <TableHead>Game Mode</TableHead>
+                                <TableHead>Difficulty</TableHead>
+                                <TableHead>Players</TableHead>
+                                <TableHead></TableHead>
                             </TableRow>
-                        ) : rooms.length === 0 ? (
-                            <TableRow>
-                                <TableCell colSpan={5} className="text-center">
-                                    No rooms available
-                                </TableCell>
-                            </TableRow>
-                        ) : (
-                            rooms.map((room: GameRoom) => (
-                                <TableRow
-                                    key={room.roomId}
-                                    className="text-center"
-                                    onClick={() => {
-                                        window.location.href = `/room/${room.roomId}`;
-                                    }}
-                                >
-                                    <TableCell>
-                                        {room.roomName.length > 10
-                                            ? `${room.roomName.slice(0, 10)}...`
-                                            : room.roomName}
-                                    </TableCell>
-                                    <TableCell>{room.roomGame}</TableCell>
-                                    <TableCell>{room.roomDifficulty}</TableCell>
-                                    <TableCell>{room.roomUsers.length}</TableCell>
-                                    <TableCell>
-                                        <LockKeyhole
-                                            size={15}
-                                            className={room.isRoomPublic ? "hidden" : ""}
-                                        />
-                                        <LockKeyholeOpen
-                                            size={15}
-                                            className={room.isRoomPublic ? "" : "hidden"}
-                                        />
+                        </TableHeader>
+                        <TableBody>
+                            {loading ? (
+                                <TableRow>
+                                    <TableCell colSpan={5} className="text-center">
+                                        <Spinner size="lg" className="bg-black dark:bg-white" />
                                     </TableCell>
                                 </TableRow>
-                            ))
-                        )}
-                    </TableBody>
-                </Table>
+                            ) : (
+                                rooms.map((room: GameRoom) => (
+                                    <TableRow
+                                        key={room.roomId}
+                                        className="text-center"
+                                        onClick={() => {
+                                            window.location.href = `/room/${room.roomId}`;
+                                        }}
+                                    >
+                                        <TableCell>
+                                            {room.roomName.length > 10
+                                                ? `${room.roomName.slice(0, 10)}...`
+                                                : room.roomName}
+                                        </TableCell>
+                                        <TableCell>{room.roomGame}</TableCell>
+                                        <TableCell>{room.roomDifficulty}</TableCell>
+                                        <TableCell>{room.roomUsers.length}</TableCell>
+                                        <TableCell>
+                                            <LockKeyhole
+                                                size={15}
+                                                className={room.isRoomPublic ? "hidden" : ""}
+                                            />
+                                            <LockKeyholeOpen
+                                                size={15}
+                                                className={room.isRoomPublic ? "" : "hidden"}
+                                            />
+                                        </TableCell>
+                                    </TableRow>
+                                ))
+                            )}
+                        </TableBody>
+                    </Table>
+                )}
             </CardContent>
         </Card>
     );
